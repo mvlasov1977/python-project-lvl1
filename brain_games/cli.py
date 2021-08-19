6ba1b9d47047913296836c055367cd5482805a68
Maksim <mvlasov1977@gmail.com>
2021-08-19 12:42:53 +0300

    added greeting and name prompt

---
+++ brain_games/cli.py
@@ -0,0 +1,7 @@
+# file cli.py
+
+import prompt
+
+def welcome_user(greeting):
+    name = prompt.string( greeting )
+    return name
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
+from brain_games.cli import welcome_user
+
 _WELCOME = 'Welcome to the Brain Games!'
+_WELCOME1 = 'May I have your name? '
+_RESPOND = 'Hello, {}!'
 
 # print welcome message :)
 def welcome_print(txt):
@@ -9,7 +13,7 @@ def welcome_print(txt):
 # define function main
 def main():
     welcome_print(_WELCOME)
-
+    print(_RESPOND.format(welcome_user(_WELCOME1)))
 # detect use type 
 if __name__ == '__main__':
      main()
