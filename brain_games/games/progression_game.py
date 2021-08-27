867ae190db8ab4ea2efd11c8291ef1ba378995be
Maksim <mvlasov1977@gmail.com>
2021-08-27 16:35:51 +0300

    Step8 add brain-progression

---
+++ brain_games/games/progression_game.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+from random import randint
+from brain_games.brain_cli import cli
+
+_RULE = 'What number is missing in the progression?'
+
+
+# initial parameters section
+
+_RANDOM_RANGE_FIRST_NUMBER = (1, 30)  # rand fst number for randint function
+_RANDOM_RANGE_INCREMENT_NUMBER = (1, 10)  # rand increment for randint function
+_NUM_OF_CORR_ANSWERS = 3  # required number of correct answers
+_LENGTH_OF_PROGRESSION = 10  # length of arithmetic progression
+_RANDOM_RANGE_SECRET_NUMBER = (1, _LENGTH_OF_PROGRESSION)  # rand sec num range
+_SECRET_MASK = '..'  # secret mask indicate secret number progression
+
+
+# generate a random number
+
+def get_random_int(range):
+    begin, end = range
+    return randint(begin, end)
+
+
+# create full sentence
+
+def get_full_sentence(first_number, step, secret_number, p_length):
+    s = []
+    progression = ''
+    index = 0
+    while index < p_length:
+        element = first_number + index * step
+        if index != 0:
+            progression = progression + ' '
+        if index == secret_number - 1:
+            progression = progression + _SECRET_MASK
+            secret_element = element
+        else:
+            progression = progression + str(element)
+        index += 1
+    s.append(progression)
+    s.append(str(secret_element))
+    return s
+
+
+# define function main
+
+def main():
+    item_count = 0
+    s = []
+    while item_count < _NUM_OF_CORR_ANSWERS:
+        first_number = get_random_int(_RANDOM_RANGE_FIRST_NUMBER)
+        step = get_random_int(_RANDOM_RANGE_INCREMENT_NUMBER)
+        secret_number = get_random_int(_RANDOM_RANGE_SECRET_NUMBER)
+        len_prg = _LENGTH_OF_PROGRESSION
+        s.append(get_full_sentence(first_number, step, secret_number, len_prg))
+        item_count += 1
+    cli(s, _RULE)
+    return None
+
+
+# detect use type
+
+if __name__ == '__main__':
+    main()
---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+from brain_games.games.progression_game import main as main_progression
+
+
+# execute calc game
+
+def main():
+    main_progression()
+    return None
+
+
+# detect use type
+
+
+if __name__ == '__main__':
+    main()
